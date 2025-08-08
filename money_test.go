7ca116f04c7bf17add206c3819e843b2e0458a1b
zy99978455-otw <17701139970@163.com>
2025-08-08 10:00:44 +0800

    first commit

---
+++ money_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+func TestMultipliationInDollars(t *testing.T) {
+	fiver := Money{amount: 5, currency: "USD"}
+	actualResult := fiver.Times(2)
+	expectedResult := Money{amount: 10, currency: "USD"}
+
+	assertEqual(t, expectedResult, actualResult)
+}
+
+func TestMultipliationInEuros(t * testing.T) {
+	tenEuros := Money{amount: 10, currency: "EUR"}
+	actualResult := tenEuros.Times(2)
+	expectedResult := Money{amount: 20, currency: "EUR"}
+
+	assertEqual(t, expectedResult, actualResult)
+}
+
+func TestDivision(t *testing.T) {
+	originalMoney := Money{amount: 4002, currency: "KRW"}
+	actualResult := originalMoney.Divide(4)
+	expectedResult:= Money{amount: 1000.5, currency: "KRW"}
+
+	assertEqual(t, expectedResult, actualResult)
+}
+
+func assertEqual(t *testing.T, expected Money, actual Money) {
+	if expected != actual {
+		t.Errorf("Expected %+v Got %+v", expected, actual)
+	}
+}
